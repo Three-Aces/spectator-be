597982543f979a47fd49949a9b99f5649038ad14
nyakamwe <nyakamweaimable@gmail.com>
2022-09-28 08:16:45 +0200

    chore(db-configuration): Configuring database with sequelize ORM

---
+++ src/errors/index.js
@@ -0,0 +1,1 @@
+/* This will holds all cntrollers */
--- src/server.js
+++ src/server.js
@@ -3,13 +3,28 @@ import 'dotenv/config';
 import Routes from './routes'
 import swaggerUI from 'swagger-ui-express'
 import swaggerDocs from "./documentation";
+import DB from './database/models';
+import cors from 'cors';
 
 const app = express();
 
 const port = process.env.PORT || 7000;
 
+app.use(cors());
+app.use(express.json());
+
+const {sequelize} = DB;
+sequelize
+    .authenticate()
+    .then(() => {
+        console.log('DB CONNECTED...');
+    })
+    .catch((err) => {
+        console.log('Unable to connect to the database: ', err);
+});
+
 app.get('/', (req, res)=>{
-    res.send('get all spectats')
+    res.send('get all spectators')
 })
 app.use('/api/v1', Routes)
 app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocs));
