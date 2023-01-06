0f8c773285a0fd14a6ecc05445bba1813695bce6
nyakamwe <nyakamweaimable@gmail.com>
2023-01-06 16:21:26 +0200

    add authentication on chat

--- src/controllers/chatControllers.js
+++ src/controllers/chatControllers.js
@@ -2,7 +2,7 @@ import chatControllers from '../services/chatServices'
 import {Chat} from '../database/models'
 
 export const findAllChats = async(req, res)=>{
-    const chats = await Chat.findAll({order:['timeStamp']})
+    const chats = await Chat.findAll({order:[['createdAt', 'ASC']]})
     return res.status(200).json({
         count: chats.length,
         chats
--- src/services/chatServices.js
+++ src/services/chatServices.js
@@ -1,7 +1,8 @@
 import socket from "socket.io";
 import { server } from "../server";
 import {Chat} from "../database/models"
-
+import verifyToken from "../helpers/verifyToken";
+let decodedToken;
 // Socket setup
 const io = socket(server,{
     cors: {
@@ -10,13 +11,47 @@ const io = socket(server,{
       }
 });
 
+// registering authentication middleware
+// middleware for authentication
+io.use(async (socket, next) => {
+    const { token } = socket.handshake.auth;
+    try {
+      if (token) {
+        decodedToken = await verifyToken(token)
+        socket.decodedToken = decodedToken;
+        // console.log('cred', decodedToken.user)
+        return next();
+      }
+      throw new Error("User not authorized ");
+    } catch (error) {
+      return next(error);
+    }
+});
+
+let onlineUsers = 0;
+export const ipsconnected = {};
+
 io.on('connection', (socket)=>{
     console.log('user connected', socket.id)
+    const user = socket.decodedToken.user
+    
+    const connectedUser = socket.id; // for real app use user.id or socket.handshake.address
+    if (!ipsconnected.hasOwnProperty(connectedUser)) {
+        ipsconnected[connectedUser] = {
+        socket,
+        user: socket.decodedToken.user
+        };
+        onlineUsers += 1;
+        io.emit("online", onlineUsers);
+    }
+    // associate connected socket to user
+    io.to(socket.id).emit("onLoad", user);
     
     socket.on('message', async(data)=>{
         console.log('message', data)
         const savedChat = await Chat.create({
             message: data,
+            userId: user.id,
             timeStamp: Date.now(),
         })
 
